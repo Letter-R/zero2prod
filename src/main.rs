7724e0a69c32399a4a93b00b447e938fa22ada33
lcflscs <lcflscs@163.com>
2022-08-24 22:53:36 +0800

    binding port 0

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+use std::net::TcpListener;
+
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run().unwrap().await
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    println!("{}", format!("http://127.0.0.1:{}", port));
+    let server = zero2prod::run(listener)
+        .expect("Failed to bind address");
+    server.await
 }
