fc5e97d95108f352236e89643e023f837ea78c23
lcflscs <lcflscs@163.com>
2022-08-26 21:28:55 +0800

    3.7 finish, 400 is from the deserialize fail

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
 use std::net::TcpListener;
 
-use zero2prod::run;
-
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind random port");
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     println!("{}", format!("http://127.0.0.1:{}", port));
-    let server = zero2prod::run(listener)
-        .expect("Failed to bind address");
+    let server = zero2prod::run(listener).expect("Failed to bind address");
     server.await
 }
