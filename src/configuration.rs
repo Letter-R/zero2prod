99a08a894f2fa764f800a7a1509018f82f372ec6
lcflscs <lcflscs@163.com>
2022-08-28 21:00:19 +0800

    3.8.5.2 change the config merge method

---
+++ src/configuration.rs
@@ -0,0 +1,27 @@
+use config::{Config, File};
+
+
+
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16
+}
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let settings = Config::builder()
+        .add_source(File::with_name("configuration.yaml"))
+        .build()
+        .unwrap();
+
+    settings.try_deserialize()
+}
